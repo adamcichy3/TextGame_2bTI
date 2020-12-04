8332b73406f82c94c747c2f1b28c9eb9d7981a4a
Your Name <adam.cichy3@gmail.com>
2020-12-04 14:59:15 +0100

    Changed name of scene // Added new script TextGame

---
+++ TextGame_2bTI/Assets/Scripts/TextGame.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TextGame : MonoBehaviour
+{
+    // w tym miejscu deklarujemy ZMIENNE GLOBALNE
+    void Start()
+    {
+        // Komunikat powitalny dla gracza
+        // Instrukcja co uzytkownik ma zrobic
+        // Najwieksza liczba jaka mozesz wprowadzic to:
+        // Najmniejsza...
+        // Pytanie od komputera czy twoja liczba... jest wieksza od tyle i tyle
+        // Aby odpowiedziec na pytanie nacisnij: Gorna strzalka =  Dolna= Enter=
+        // Zdefiniowac 3 zmienne typu integer - dla najmniejsze liczby, najwiekszej i dla strzalu (guess)
+        // I/O -> Input  -> Output
+        // int string bool float double char(kar)
+        
+        
+    }
+    
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+
+        }
+        // dodac dwie kolejne instrukcje warunkowe z odpowiednimi klawiszami (strzalka dol, enter)
+        // do instrukcji warunkowych wlóżcie odpowiednie instrukcje dotyczac przypisywania wartosci guess do min lub max
+        // przy strzalkach musi byc guess na nowo wyliczany
+        // przy ifach z strzalkami wyswietl komunikat "
+      
+
+
+       
+    }
+}
